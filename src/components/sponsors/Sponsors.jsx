a9a263b3877bf7e0272d2fb68f773573c8b2494a
eslamal3kda <al3kda1999@gmail.com>
2025-01-23 00:36:18 +0200

    add sponsors section

---
+++ src/components/sponsors/Sponsors.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Sponsors() {
+    return (
+        <>
+            <div className="sponsors-section x-padding">
+                <div className="sponsors-section-icons">
+                    <img src="./src/assets/images/Sponsors/html-logo.png" alt="" />
+                </div>
+                <div className="sponsors-section-icons">
+                    <img src="./src/assets/images/Sponsors/css-logo.png" alt="" />
+                </div>
+                <div className="sponsors-section-icons">
+                    <img src="./src/assets/images/Sponsors/javascript-logo.png" alt="" />
+                </div>
+                <div className="sponsors-section-icons">
+                    <img src="./src/assets/images/Sponsors/sass-logo.png" alt="" />
+                </div>
+                <div className="sponsors-section-icons">
+                    <img src="./src/assets/images/Sponsors/react-logo.png" alt="" />
+                </div>
+            </div>
+        </>
+    );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import ContactBar from "../../components/contactBar/ContactBar";
+import Sponsors from "../../components/sponsors/Sponsors";
 
 export default function HomePage() {
     return (
         <>
             <main className="home-page ">
                 <section className="page-test hero-section x-padding"><div className="hero-section"></div></section>
+                <Sponsors />
                 <ContactBar />
             </main>
         </>
